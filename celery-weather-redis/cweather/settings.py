0d37cc3a78ff5974a34d7024a58fd258bc4b6b0d
Paul Bailey <paul@neutron.studio>
2023-11-04 18:38:29 -0500

    docker compose setup

--- celery-weather-redis/cweather/settings.py
+++ celery-weather-redis/cweather/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +76,7 @@ WSGI_APPLICATION = 'cweather.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://postgres:passyword@localhost:5432/asyncdj')
 }
 
 
