dcfa276f48b4e6189f8171fdcc825022b75b7721
Paul Bailey <paul@neutron.studio>
2023-12-04 20:44:41 -0600

    file streaming

--- file-streamer/test_client.py
+++ file-streamer/test_client.py
@@ -8,23 +8,28 @@ def run_tests (video_url="http://localhost:8000/video", content_length=121283919
   print('Full Length Test:', len(resp.content), content_length)
   assert resp.headers['Content-Length'] == str(content_length)
   assert len(resp.content) == content_length
+  assert resp.status_code == 200
 
   resp = httpx.get(video_url, headers={'Range': 'bytes=0-5000'})
   print('Chunk Test:', len(resp.content), 5001)
   assert len(resp.content) == 5001
+  assert resp.status_code == 206
 
   resp = httpx.get(video_url, headers={'Range': 'bytes=0-1023'})
   print('Small Chunk Test:', len(resp.content), 1024)
   assert len(resp.content) == 1024
+  assert resp.status_code == 206
 
   resp = httpx.get(video_url, headers={'Range': 'bytes=1024-2047'})
   print('Jump Chunk Test:', len(resp.content), 1024)
   assert len(resp.content) == 1024
+  assert resp.status_code == 206
 
   end = content_length - 1024
   resp = httpx.get(video_url, headers={'Range': f'bytes=1024-{end}'})
   print('Large Jump Chunk Test:', len(resp.content), end - 1023)
   assert len(resp.content) == (end - 1023)
+  assert resp.status_code == 206
 
 if __name__ == "__main__":
   run_tests()
